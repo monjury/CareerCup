b3a7477d7f72c785c2f289b93eccd50c8e5574ae
sampriya <monjury@gmail.com>
2015-08-05 23:33:38 -0700

    reverse words

---
+++ Zynga/src/reverse/words/ReverseWords.java
@@ -0,0 +1,40 @@
+package reverse.words;
+
+/*Reverse words : "I love to play" becomes "play to love I" in place.*/
+
+public class ReverseWords {
+	// single space
+	// no period
+
+	public char[] reverse(char[] input) {
+		reverseWord(input, 0, input.length);
+		int start = 0;
+		int end = 0;
+		while (end < input.length) {
+			if (input[end] == ' ') {
+				reverseWord(input, start, end);
+				start = end + 1;
+			}
+			end++;
+		}
+		return input;
+	}
+
+	public char[] reverseWord(char[] input, int start, int end) {
+		int init = start;
+		int mid = (start + end) / 2;
+		for (int i = start; i < mid; i++) {
+			char temp = input[i];
+			input[i] = input[end - 1 - (i-init)];
+			input[end - 1 - (i-init)] = temp;
+		}
+		return input;
+	}
+
+	public static void main(String[] args) {
+		ReverseWords rw = new ReverseWords();
+		char[] output = rw.reverse("I love to play".toCharArray());
+		String print = String.valueOf(output);
+		System.out.println(print);
+	}
+}
